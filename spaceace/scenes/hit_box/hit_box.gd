6b5108f8b6d9d3235f3706c66436653b38cd3874
Devin Alexander <devinmalexander@gmail.com>
2025-01-27 12:54:58 -0600

    Shield scene

--- spaceace/scenes/hit_box/hit_box.gd
+++ spaceace/scenes/hit_box/hit_box.gd
@@ -14,9 +14,10 @@ func deactivate() -> void:
 	collision_shape_2d.call_deferred("set_disabled", true)
 	
 	
+
 func get_damage() -> int:
 	return damage
-	
-	
+
+
 func _on_area_entered(area: Area2D) -> void:
-	pass
+	pass # Replace with function body.
--- spaceace/scenes/power_up/power_up.gd
+++ spaceace/scenes/power_up/power_up.gd
@@ -10,36 +10,37 @@ enum PowerUpType { HEALTH, SHIELD }
 const SPEED: float = 80.0
 
 
-const TEXTURES: Dictionary = {
+const TEXURES: Dictionary = {
 	PowerUpType.HEALTH: preload("res://assets/misc/powerupGreen_bolt.png"),
-	PowerUpType.SHIELD: preload("res://assets/misc/shield_gold.png")
+	PowerUpType.SHIELD: preload("res://assets/misc/shield_gold.png"),
 }
 
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var sound: AudioStreamPlayer2D = $Sound
 
+
 var _power_up_type: PowerUpType = PowerUpType.SHIELD
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	sprite_2d.texture = TEXTURES[_power_up_type]
+	sprite_2d.texture = TEXURES[_power_up_type]
 	SoundManager.play_powerup_deploy_sound(sound)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y += delta * SPEED
-	
-	
+
+
 func set_power_up_type(pu: PowerUpType) -> void:
 	_power_up_type = pu
-	
-	
+
+
 func get_power_up_type() -> PowerUpType:
 	return _power_up_type
-	
-	
+
+
 func _on_area_entered(area: Area2D) -> void:
 	queue_free()
