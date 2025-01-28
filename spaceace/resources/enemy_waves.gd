3957cd1774c8f56a6df7e7df8312000a3a03815b
Devin Alexander <devinmalexander@gmail.com>
2025-01-28 12:37:52 -0600

    Wave Manager

--- spaceace/resources/enemy_waves.gd
+++ spaceace/resources/enemy_waves.gd
@@ -6,9 +6,10 @@ class_name EnemyWaves
 
 @export var waves: Array[EnemyWave]
 
+
 func get_wave_for_wave_count(wc: int) -> EnemyWave:
 	return waves[wc % waves.size()]
-	
-	
+
+
 func wave_is_start(wc: int) -> bool:
 	return wc % waves.size() == 0
